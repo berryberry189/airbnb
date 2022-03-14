1aa799be087f7b8e0dd9d9977a412d76aedf6304
berryberry189 <berryberry189@naver.com>
2022-03-14 22:03:14 +0900

    CommonCode entity 생성

---
+++ src/main/java/com/grace/airbnb/model/CommonCode.java
@@ -0,0 +1,33 @@
+package com.grace.airbnb.model;
+
+import com.grace.airbnb.util.BooleanToYNConverter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "common_code")
+@Getter
+@NoArgsConstructor
+public class CommonCode {
+
+    @Id
+    @Column(name = "code_type", columnDefinition = "varchar(200) comment '코드 타입'")
+    private String codeType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_code_type")
+    private CommonCode parentCodeType;
+
+    @Column(name = "code_order", columnDefinition = "int comment '코드 순서'")
+    private int codeOrder;
+
+    @Column(name = "value", nullable = false, columnDefinition = "varchar(200) comment '코드 값'")
+    private String value;
+
+    @Convert(converter= BooleanToYNConverter.class)
+    @Column(name = "display_yn", nullable = false, columnDefinition = "varchar(1) default 'Y' comment '노출 여부'")
+    private boolean displayYN;
+
+}
